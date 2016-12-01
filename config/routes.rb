6d712d28025e47db0a7d4f1d45ea6d0e389e6c84
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-12-01 16:54:09 -0500

    Generated users and posts models and controllers. Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get resources :users, only: [:new, :create, :edit, :update] do
+    get resources :posts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
