f7d3cbd0d7eef834535fa54fa456e527b9b4159e
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-12-04 17:31:37 -0500

    Added in nicer looking pagination for posts

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  load_and_authorize_resource :user, parent: false
+
   def edit
     @user = User.find(params[:id])
   end
--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -3,10 +3,31 @@ require 'test_helper'
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
 
   test "can get edit for own profile" do
-    post session_path, params: {email: "hagrid@thehutt.com", password: "password"}
+    post session_path, params: { user_name: "pink_umbrella", password: "password" }
     get edit_profile_path(users(:hagrid).id)
     assert_response :success
     assert_select "form"
   end
 
+  test "can get edit password for own profile" do
+    post session_path, params: { user_name: "pink_umbrella", password: "password" }
+    get edit_password_path(users(:hagrid).id)
+    assert_response :success
+    assert_select "form"
+  end
+
+  test "can't edit profile of another user" do
+    post session_path, params: { user_name: "pink_umbrella", password: "password" }
+    get edit_profile_path(users(:fang).id)
+    assert_response :redirect
+    assert_equal "You are not authorized to access this page.", flash[:danger]
+  end
+
+  test "can edit own profile info" do
+    hagrid_id = users(:hagrid).id
+    post session_path, params: { user_name: "pink_umbrella", password: "password" }
+    patch profile_path(hagrid_id), params: { user: { email: "hagrid@hogwarts.com" } }
+    assert_response :redirect
+    assert_equal "hagrid@hogwarts.com", User.find(hagrid_id).email
+  end
 end
